71f332467f01279071f5a4d86bc308079b63a6f9
lenuhhh <161879235+lenuhhh@users.noreply.github.com>
2024-08-27 18:21:23 +0300

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,31 +1,31 @@
 const customer = {
-  username: "Mango",
+  username: 'Mango',
   balance: 24000,
   discount: 0.1,
-  orders: ["Burger", "Pizza", "Salad"],
-
+  orders: ['Burger', 'Pizza', 'Salad'],
+  
   getBalance() {
-    return this.balance; 
+    return this.balance;
   },
   getDiscount() {
-    return this.discount; 
+    return this.discount;
   },
   setDiscount(value) {
     this.discount = value;
   },
   getOrders() {
-    return this.orders; 
+    return this.orders;
   },
   addOrder(cost, order) {
     this.balance -= cost - cost * this.discount;
     this.orders.push(order);
   },
-
 };
+
 
 
 customer.setDiscount(0.15);
 console.log(customer.getDiscount()); 
-customer.addOrder(5000, "Steak");
-console.log(customer.getBalance());
+customer.addOrder(5000, 'Steak');
+console.log(customer.getBalance()); 
 console.log(customer.getOrders()); 
