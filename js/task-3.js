e93a09a874aafd0f100f75390a7aa973c0c0374e
lenuhhh <161879235+lenuhhh@users.noreply.github.com>
2024-08-27 18:29:11 +0300

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,40 +1,49 @@
-class StringBuilder {
-    #value; 
+const sortByDescendingFriendCount = users =>
+  users.toSorted(
+    (firstUserQuantityOfFriends, secondUserQuantityOfFriends) =>
+      secondUserQuantityOfFriends.friends.length -
+      firstUserQuantityOfFriends.friends.length,
+  );
 
-    constructor(initialValue = '') {
-        this.#value = initialValue;
-    }
-
-    getValue() {
-        return this.#value;
-    }
-
-    padEnd(endString) {
-        this.#value = this.#value + endString;
-    }
-
-    padStart(startString) {
-        this.#value = startString + this.#value;
-    }
-
-    padBoth(padString) {
-        this.#value = padString + this.#value + padString;
-    }
-}
-
-
-let builder = new StringBuilder(".");
-
-console.log(builder.getValue()); 
-
-builder.padStart("^");
-console.log(builder.getValue()); 
-
-builder.padEnd("^");
-console.log(builder.getValue());  
-
-builder.padBoth("=");
-console.log(builder.getValue());  
+console.log(
+  sortByDescendingFriendCount([
+    {
+      name: 'Moore Hensley',
+      friends: ['Sharron Pace'],
+      gender: 'male',
+    },
+    {
+      name: 'Sharlene Bush',
+      friends: ['Briana Decker', 'Sharron Pace'],
+      gender: 'female',
+    },
+    {
+      name: 'Ross Vazquez',
+      friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+      gender: 'male',
+    },
+    {
+      name: 'Elma Head',
+      friends: ['Goldie Gentry', 'Aisha Tran'],
+      gender: 'female',
+    },
+    {
+      name: 'Carey Barr',
+      friends: ['Jordan Sampson', 'Eddie Strong'],
+      gender: 'male',
+    },
+    {
+      name: 'Blackburn Dotson',
+      friends: ['Jacklyn Lucas', 'Linda Chapman'],
+      gender: 'male',
+    },
+    {
+      name: 'Sheree Anthony',
+      friends: ['Goldie Gentry', 'Briana Decker'],
+      gender: 'female',
+    },
+  ]),
+);
 
 
 
